dbfe26f6a4712135c315db03f16db5ca9ea4da80
Oleg Taranenko <olegtaranenko@gmail.com>
2013-02-18 06:21:24 +0100

    get socket.io working

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-var io = require('socket.io').listen(80);
+var express = require('express');
+var path = require("path");
+var http = require('http');
+var app = express();
+var pub_dir = path.join(__dirname, 'client');
+var server = http.createServer(app);
 
+server.listen(8000);
+
+app.configure(function() {
+	app.use(express.static(pub_dir));
+});
+
+var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
 	socket.emit('news', { hello: 'world' });
 	socket.on('my other event', function (data) {
