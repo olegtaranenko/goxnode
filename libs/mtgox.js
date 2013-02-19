b50e1501d56a9d2de9e4748c7d78b8ac714eb71a
Oleg Taranenko <olegtaranenko@gmail.com>
2013-02-19 21:37:29 +0100

    integrate mtgox.js lib

---
+++ libs/mtgox.js
@@ -0,0 +1,111 @@
+var util = require('util');
+var fs = require('fs');
+var events = require('events');
+var io = require('socket.io-client');
+
+var MTGOX_SOCKET_URL = 'https://socketio.mtgox.com/mtgox';
+var MTGOX_CHANNELS = [];
+var CURRENCY = 'USD';
+var configFilename = 'mtgoxconfig.json';
+
+try {
+  var config = JSON.parse(fs.readFileSync(__dirname + '/' + configFilename));
+  if (Array.isArray(config.channels)) {
+    MTGOX_CHANNELS = config.channels;
+  }
+  var currency = config ? config.currency : false;
+  if (currency) {
+    CURRENCY = currency;
+    MTGOX_SOCKET_URL += '?Currency=' + currency;
+  }
+}
+catch(ex) {
+  util.debug(util.inspect(ex));
+  util.debug('Failed to parse %1. No channels available.', configFilename);
+}
+
+var getChannel = function(key, currency) {
+  currency = currency || CURRENCY;
+
+  return MTGOX_CHANNELS.filter(function(channel) {
+    var channelCurrency = channel.currency || CURRENCY;
+    return ((channel.key == key || channel.private == key) && channelCurrency == currency);
+  })[0];
+};
+
+
+var MtGoxClient = function() {
+  events.EventEmitter.call(this);
+  var self = this;
+  console.log('try to connect to ', MTGOX_SOCKET_URL);
+  var socket = io.connect(MTGOX_SOCKET_URL);
+
+  socket.on('message', function(raw) {
+    // Emit raw data
+    var data = raw;
+    self.emit('data', data);
+
+    // Emit messages
+      var message = data;
+      self.emit('message', message);
+
+      if (message.op == 'subscribe') {
+        self.emit('subscribe', message);
+      }
+
+      if (message.op == 'unsubscribe') {
+        self.emit('unsubscribe', message);
+      }
+
+      if (message.op == 'private') {
+        self.emit(message.private, message);
+      }
+  });
+
+  socket.on('error', function(error) {
+    util.debug(error);
+    self.emit('error', error);
+  });
+
+  socket.on('connect', function() {
+    self.emit('connect');
+  });
+
+  socket.on('disconnect', function() {
+    self.emit('disconnect');
+  });
+
+  self.subscribe = function(channel) {
+    var message = {
+      "op": "subscribe",
+      "channel": channel
+    };
+    socket.send(message);
+  };
+
+  self.unsubscribe = function(channel) {
+    var message = {
+      "op": "unsubscribe",
+      "channel": channel
+    };
+    console.log('message', channel);
+    socket.send(message);
+  };
+
+  self.close = function(timeout) {
+  };
+
+  // Allow access to underlying socket
+  self.socket = socket;
+};
+
+util.inherits(MtGoxClient, events.EventEmitter);
+
+exports.MtGoxClient = MtGoxClient;
+exports.CHANNELS = MTGOX_CHANNELS;
+exports.URL = MTGOX_SOCKET_URL;
+exports.getChannel = getChannel;
+
+exports.connect = function() {
+  return new MtGoxClient();
+};
--- server.js
+++ server.js
@@ -2,19 +2,53 @@ var express = require('express');
 var path = require("path");
 var http = require('http');
 var app = express();
-var pub_dir = path.join(__dirname, 'client');
+var pub_dir = path.join(__dirname, 'public');
 var server = http.createServer(app);
+var mtgox = require('./libs/mtgox');
+var client = null; // connect later after loading depth from clarkmoody
+//var client = mtgox.connect();
+
+// load clarkmoody data local;
+var util = require('util');
+var fs = require('fs');
+/*
+var dataJsonName = __dirname + '/api/v0/depth/USD.json';
+try {
+  var config = JSON.parse(fs.readFile(dataJsonName, function() {
+
+  }));
+}
+catch(ex) {
+  util.debug(util.inspect(ex));
+  util.debug('Failed to parse %1. No channels available.', dataJsonName);
+}
+
+*/
+
+
+try {
+  var config = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+}
+catch(ex) {
+  util.debug(util.inspect(ex));
+  util.debug('Failed to parse config.json. No channels available.');
+}
+
+
 
 server.listen(8000);
 
 app.configure(function() {
-	app.use(express.static(pub_dir));
+  app.use('/api/v0', function() {
+    debugger;
+  });
+  app.use(express.static(pub_dir));
 });
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
 });
