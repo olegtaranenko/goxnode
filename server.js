ff6fff30001c66ca000622e60d31b8d232488b73
Oleg Taranenko <olegtaranenko@gmail.com>
2013-03-06 07:05:21 +0100

    try to go live with websocket auth command, failed

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+//var crypto = require('crypto');
 var path = require("path");
+"request": "2.11.4",
 var http = require('http');
 var app = express();
 var pub_dir = path.join(__dirname, 'public'),
@@ -9,11 +11,6 @@ var pub_dir = path.join(__dirname, 'public'),
 // web server
 var server = http.createServer(app);
 
-// library from mtgox-socket-client
-var mtgox = require('./lib/mtgox');
-//var client = null; // connect later after loading depth from clarkmoody
-var clientMtgox = mtgox.connect();
-
 var util = require('util');
 var fs = require('fs');
 //setup my own implementation of the logger instead of used in socket.io
@@ -21,35 +18,53 @@ var mylogger = require('./lib/mylogger'),
   Log = new mylogger();
 
 
-clientMtgox.on('connect', function() {
-  Log.info('Connected to MtGox!');
-//  clientMtgox.unsubscribe('dbf1dee9-4f2e-4a08-8cb7-748919a71b21');
-});
-
-
-// load clarkmoody data local;
-// load configuration for the web server instance
-// i.e. current user, default currency etc
-/*
-var dataJsonName = __dirname + '/api/v0/depth/fulldepth.json';
 try {
-  var fullDepth = JSON.parse(fs.readFileSync(dataJsonName));
-
-  util.debug(fullDepth);
-}
-catch(ex) {
-  util.debug(util.inspect(ex));
-}
-*/
-
-
-try {
-  var config = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+  var configFile = __dirname + '/config.json';
+  var configText = fs.readFileSync(configFile).toString();
+  var config = JSON.parse(configText);
+  var apiKey = config.security.apiKey;
+  var secret = config.security.secret;
 }
 catch(ex) {
   util.debug(util.inspect(ex));
   util.debug('Failed to parse config.json. No channels available.');
 }
+
+
+
+// library from mtgox-socket-client
+var mtgox = require('./lib/mtgox');
+var clientMtgox = mtgox.connect({
+  apiKey: apiKey,
+  secret: secret
+});
+
+
+clientMtgox.on('connect', function() {
+  Log.info('Connected to MtGox!');
+
+  this.unsubscribe('dbf1dee9-4f2e-4a08-8cb7-748919a71b21');
+  this.unsubscribe('d5f06780-30a8-4a48-a2f8-7ed181b4a13f');
+  this.unsubscribe('24e67e0d-1cad-4cc0-9e7a-f8523ef460fe');
+  this.phpCall();
+//  this.queryApi('private\/info');
+
+// load configuration for the web server instance
+// i.e. current user, default currency etc
+  var dataJsonName = __dirname + '/api/v0/depth/fulldepth.json';
+  try {
+    var fullDepth = JSON.parse(fs.readFileSync(dataJsonName));
+
+    Log.info('Full Depth is loaded');
+  }
+  catch(ex) {
+    Log.error(util.inspect(ex));
+  }
+
+});
+
+
+
 
 // on-fly preprocessor for less/css bowels
 var lessMiddleware = require('less-middleware');
