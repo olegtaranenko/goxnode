f5990a609132b14614dc2f6990b67197f6380895
Oleg Taranenko <olegtaranenko@gmail.com>
2013-03-13 09:26:31 +0100

    changes regards lostPrivateChannel

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ var fs = require('fs');
 var mylogger = require('./lib/mylogger'),
   Log = new mylogger();
 
+// create connection via socket.io layer
+var io = require('socket.io').listen(server, {
+  logger: new mylogger,
+  'Log level': 3
+});
+
+
 var configJson = __dirname + '/config.json',
   config = require(configJson),
   apiKey = config.security.apiKey,
@@ -25,13 +32,21 @@ var configJson = __dirname + '/config.json',
 
 client.strategies = strategies;
 
+var lastPrivateChannel = {
+  started: null, // date
+  updated: null, // date
+  guid: null,
+  subscribed: false
+};
+
+
 // library from mtgox-socket-client
 var mtgox = require('./lib/mtgox');
 var clientMtgox = mtgox.connect({
+  lastPrivateChannel: lastPrivateChannel,
   apiKey: apiKey,
   secret: secret
 });
-
 
 
 clientMtgox.on('connect', function() {
@@ -40,6 +55,10 @@ clientMtgox.on('connect', function() {
   this.unsubscribe('dbf1dee9-4f2e-4a08-8cb7-748919a71b21');
   this.unsubscribe('d5f06780-30a8-4a48-a2f8-7ed181b4a13f');
   this.unsubscribe('24e67e0d-1cad-4cc0-9e7a-f8523ef460fe');
+
+  lastPrivateChannel.subscribed = false;
+
+  subscribePrivateChannel(lastPrivateChannel);
 
 
 // load configuration for the web server instance
@@ -80,17 +99,11 @@ app.configure(function() {
 });
 
 
-// create connection via socket.io layer
-var io = require('socket.io').listen(server, {
-  logger: new mylogger,
-  'Log level': 3
-});
-
-
 //io.set('Log level', 4);
 //io.set('heartbeat interval', 120);
 //io.set('heartbeat timeout', 240);
 //io.set('close timeout', 240);
+
 
 
 // additional configuration after connecting via socket.io channel
@@ -104,8 +117,28 @@ io.sockets.on('connection', function (socket) {
     Log.debug('socket.customData => ', socket.customData);
   }
 
-  executeClosure(socket, [retrievePrivateInfo, subscribePrivateChannel, retrieveOpenOrders], this, {});
+  executeClosure(socket, [retrievePrivateInfo, retrieveOpenOrders], this, {});
 });
+
+
+////////////////// Closure functions //////////////////////
+
+function subscribePrivateChannel(last) {
+  var subscribed = last.subscribed;
+
+  if (!subscribed) {
+    var args = arguments;
+    var idKeyPath = '1/generic/private/idkey';
+    clientMtgox.queryHttps(idKeyPath, function (err, result) {
+      if (err) {
+        Log.error('Error by call to ', idKeyPath, 'error => ', err);
+        return;
+      }
+      Log.info('Got new idKey => ', result);
+      clientMtgox.subscribePrivate(result);
+    });
+  }
+}
 
 
 function executeClosure(socket, funcs, scope, options) {
@@ -115,7 +148,6 @@ function executeClosure(socket, funcs, scope, options) {
   }
 }
 
-////////////////// Closure functions //////////////////////
 
 function retrievePrivateInfo(socket, cb, scope, options) {
   var args = arguments;
@@ -145,20 +177,6 @@ function retrieveOpenOrders(socket, cb, scope, options) {
     }
 //    Log.info('Open Orders  => ', orders, '\n', JSON.stringify(orders));
     socket.emit('ordersinfo', orders);
-    executeClosure.apply(scope || this, args);
-  });
-}
-
-function subscribePrivateChannel(socket, cb, scope, options) {
-  var args = arguments;
-  var idKeyPath = '1/generic/private/idkey';
-  clientMtgox.queryHttps(idKeyPath, function (err, result) {
-    if (err) {
-      Log.error('Error by call to ', idKeyPath, 'error => ', err);
-      return;
-    }
-    Log.info('Got new idKey => ', result);
-    clientMtgox.subscribePrivate(result, options.Id);
     executeClosure.apply(scope || this, args);
   });
 }
